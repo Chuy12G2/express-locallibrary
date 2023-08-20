3d2f2f3d1d86622490ca43b9e192a1a4d643fba5
jesus <chuygs12@gmail.com>
2023-08-20 16:07:22 -0600

    creates routes for the catalog and linked them with the controllers

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const wiki = require('./routes/wiki')
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 const app = express();
 
@@ -29,10 +28,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/wiki', wiki)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
